17d24c77d99eaadf1bf645b4bf648b079925c84f
blazejosojca <blazejosojca@gmail.com>
2017-11-15 14:21:38 +0100

    defined models, admin, serializers pt 1

--- movies_rest/movies/admin.py
+++ movies_rest/movies/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Person, Movie, Role
 
-# Register your models here.
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name']
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['title',
+                    'description',
+                    'director',
+                    'actors',
+                    'year',
+                    'ranking']
+
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ['movie',
+                    'person',
+                    'role']
--- movies_rest/movies/models.py
+++ movies_rest/movies/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
-# Create your models here.
+MOVIE_RANK = (
+    (1, "very bad"),
+    (2, "poor"),
+    (3, "average"),
+    (4, "good"),
+    (5, "masterpiece"),
+)
+
+GENRE = (
+    ('sci-fi', "science-fiction"),
+    ('fantasy', "fantasy"),
+    ('historical', "historical"),
+    ('drama', "drama"),
+    ('comedy', "comedy"),
+    ('romance', "romance"),
+    ('adult', "adult"),
+    ('cartoon', "cartoon"),
+)
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return "{} {}".format(self.first_name,
+                              self.last_name)
+
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    director = models.ForeignKey(Person,
+                                 on_delete=models.CASCADE,
+                                 related_name="movie_director")
+    actors = models.ManyToManyField(Person, through='Role')
+    year = models.IntegerField()
+    ranking = models.IntegerField(choices=MOVIE_RANK)
+    genre = models.IntegerField(choices=GENRE)
+
+    def __str__(self):
+        return self.title
+
+
+class Role(models.Model):
+    movie = models.ForeignKey(Movie, related_name="movie_role")
+    person = models.ForeignKey(Person, related_name="person_role")
+    role = models.CharField(max_length=64, null=True)
+
+    def __str__(self):
+        return "{} - {} {}".format(self.movie,
+                                   self.person,
+                                   self.role)
+
