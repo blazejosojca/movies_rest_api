0ab628783bcc506c185b81420dca78b569d6c80c
blazejosojca <blazejosojca@gmail.com>
2017-11-25 22:15:11 +0100

    changes in main urls file - added urls for app persons

--- movies_rest/movies_rest/urls.py
+++ movies_rest/movies_rest/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^movies/', include('movies.urls')),
+    url(r'^persons/', include('persons.urls')),
 ]
